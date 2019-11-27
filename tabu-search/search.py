6fbaecae068ec8aeae5b80aaba2b8b54dad0a3c9
gercom <example@ufpa.br>
2019-11-27 19:07:18 -0300

    Adds the first files for tabu search implementation

---
+++ tabu-search/search.py
@@ -0,0 +1,22 @@
+''' This file implements the Tabu Search to the modeled problem.
+'''
+
+# General variables definition
+
+# The variable that controls the loop
+stop = False
+
+# The tabu-list
+tabu = list()
+
+
+# Generating the initial solution
+
+while not stop:
+    # Neighborhood generation
+
+    # Neighbor selection
+
+    # Intermediate-term memory
+
+    # Checking termination criteria
